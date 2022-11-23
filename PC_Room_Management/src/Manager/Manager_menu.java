646efa730d400053d25e15fd0dfd109b7de850b2
HwangJM1 <yebin2495@gmail.com>
2022-11-24 02:06:11 +0900

    매니저 메뉴 빈 좌석 삭제 안되게 수정 좌석 선택 로그인 시 대여중인 좌석에 로그인 되어 있는 아이디 넘어가지 않게 수정 좌석,음식 선택 창 홈 버튼 눌렀을 때 로그아웃 되게 수정

--- PC_Room_Management/src/Manager/Manager_menu.java
+++ PC_Room_Management/src/Manager/Manager_menu.java
@@ -15,12 +15,9 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.Date;
 	
-// 관리자 로그인시 들어오는 첫 화면(좌석 확인)
+//관리자 로그인시 들어오는 첫 화면(좌석 확인)
 public class Manager_menu extends JFrame{
 
-	Connection conn = null;
-	Database db = new Database();
-	
 	public static void input_btn(JButton btn, int x, int y, int xsize, int ysize) {
 		// 버튼 생성 메소드
 		btn.setContentAreaFilled(false);
@@ -29,19 +26,23 @@ public class Manager_menu extends JFrame{
 		btn.setBounds(x, y, xsize+2, ysize+2); // 버튼 위치, 사이즈
 	}
 	
+	Connection conn = null; //DB 접속
+	Database db = new Database();
 	public Manager_menu()
 	{
-		try { 
-			 Class.forName("com.mysql.cj.jdbc.Driver");
-			 System.out.println("드라이버 검색 성공");
-		 }catch(ClassNotFoundException e) {
-			 System.err.println("드라이버 검색 실패");
-			 System.exit(0);
-		 }
-		 try {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("드라이버 검색 성공");
+			
+		}
+		catch(ClassNotFoundException e) {
+			System.err.println("드라이버 검색 실패");
+			System.exit(0);
+		}
+		try {
 			 conn = DriverManager.getConnection(
-					 "url"  // 서버 이름
-					 ,"name","pw" // 이름, 비밀번호(커넥션 정보는 깃허브에 업로드 하지 말 것)
+					 "jdbc:mysql://www.jftt.kr:49176/PC?severTimezone=UTC"  // 서버 이름
+					 ,"root","jeong_oo" // 이름, 비밀번호(커넥션 정보는 깃허브에 업로드 하지 말 것)
 					 );
 			 System.out.println("데이터베이스 연결 성공");
 		 }catch (SQLException e) {
@@ -50,16 +51,13 @@ public class Manager_menu extends JFrame{
 			 System.exit(0);
 		 }
 		
-		int size = db_amount();
-		int[] rent_num = new int[size];
-		
 		int cnt = 0;
 		String[] btn_Title = { "1", "2", "3",
 	            "4", "5", "6", "7", "8", "9","10", "11", "12", 
 				"13", "14","15", "16", "17", "18", "19","20", "21", "22", "23", "24",
 				 "25","26", "27", "28", "29","30"}; // 버튼 이름 배열
 		
-		int seat = 30;
+		int seat = 0;
 		JLabel seat_num = new JLabel("남은 좌석 " + seat + "/" + 30); 
 		seat_num.setBounds(334, 80, 200, 30);
 		seat_num.setFont(new Font("맑은 고딕", Font.BOLD, 20)); // 폰트
@@ -85,132 +83,140 @@ public class Manager_menu extends JFrame{
 		input_btn(setting_icon, 1460, 20, 40, 40);
 		add(setting_icon); // 프레임에 버튼을 붙임
 		
+
 		// 좌석 버튼
 		JButton[] seat_btn = new JButton[30];
 		//input_btn(one_seat,330, 140, 92, 86);
 		//seat_btn.setFont(new Font("맑은 고딕", Font.BOLD, 24));
-				
+		
 		for(int i = 0; i < 30; i++) // 버튼 0번 부터 29번까지 붙임
 		{
 			
 			add(seat_btn[i] = new JButton(btn_Title[i]));
-			    // 버튼 생성하여 JButton 타입의 배열에 저장
-					
+	            // 버튼 생성하여 JButton 타입의 배열에 저장
+			
 			if(i % 6 == 0)
 			{
 				cnt = 0; // 위치 조정을 위한 변수
 			}
-		        	
+     	
 			
-		    if(i <= 5)
-		    {
-		       if(i % 2 != 0 || i == 0)
-		       {
-		            input_btn(seat_btn[i],330+cnt, 132, 92, 92);
-		            seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
-		            seat_btn[i].setBackground(new Color(240, 240, 240)); // 이용 중인 좌석일 경우 나타낼 색
-		            cnt += 110;
-		       }
-		            	
-		       else if(i % 2 == 0)
-		       {
-		            input_btn(seat_btn[i],330+cnt+100, 132, 92, 92);
-		            seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
-		            seat_btn[i].setBackground(new Color(240, 240, 240));
-		            cnt += 210;
-		       }
-		            	
-		    }
-		            
-		            
-		    else if(6 <= i && i <= 11)
-		    {
-		        if(i % 2 == 0)
-		        {
-		            input_btn(seat_btn[i],330+cnt, 242, 92, 92);
-		            seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
-		            seat_btn[i].setBackground(new Color(240, 240, 240));
-		            cnt += 110;
-		        }
-		            	
-		            	
-		        else if(i % 2 != 0)
-		        {
-		            input_btn(seat_btn[i],330+cnt, 242, 92, 92);
-		            seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
-		            seat_btn[i].setBackground(new Color(240, 240, 240));
-		            cnt += 210;
-		        }
-		            	
-		    }
-		            
-		    else if(12 <= i && i <= 17)
-		    {
-		        if(i % 2 == 0)
-		        {
-		            input_btn(seat_btn[i],330+cnt, 350, 92, 92);
-		            seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
-		            seat_btn[i].setBackground(new Color(240, 240, 240));
-		            cnt += 110;
-		        }
-		            	
-		            	
-		        else if(i % 2 != 0)
-		        {
-		            input_btn(seat_btn[i],330+cnt, 350, 92, 92);
-		            seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
-		            seat_btn[i].setBackground(new Color(240, 240, 240));
-		            cnt += 210;
-		        }
-		            	
-		    }
-		            
-		    else if(18 <= i && i <= 23)
-		    {
-		        if(i % 2 == 0)
-		        {
-		            input_btn(seat_btn[i],330+cnt, 460, 92, 92);
-		            seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
-		            seat_btn[i].setBackground(new Color(240, 240, 240));
-		            cnt += 110;
-		        }
-		            	
-		            	
-		        else if(i % 2 != 0)
-		        {
-		            input_btn(seat_btn[i],330+cnt, 460, 92, 92);
-		            seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
-		            seat_btn[i].setBackground(new Color(240, 240, 240));;
-		            cnt += 210;
-		        }
-		            	
-		    }
-		            
-		    else
-		    {
-		        if(i % 2 == 0)
-		        {
-		            input_btn(seat_btn[i],330+cnt, 570, 92, 92);
-		            seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
-		            seat_btn[i].setBackground(new Color(240, 240, 240));
-		            cnt += 110;
-		        }
-		            	
-		            	
-		        else if(i % 2 != 0)
-		        {
-		            input_btn(seat_btn[i],330+cnt, 570, 92, 92);
-		            seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
-		            seat_btn[i].setBackground(new Color(240, 240, 240));
-		            cnt += 210;
-		        }
-		    }
+         if(i <= 5)
+         {
+         	if(i % 2 != 0 || i == 0)
+         	{
+         		input_btn(seat_btn[i],330+cnt, 132, 92, 92);
+             	seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
+             	seat_btn[i].setBackground(new Color(240, 240, 240)); // 이용 중인 좌석일 경우 나타낼 색
+             	cnt += 110;
+         	}
+         	
+         	else if(i % 2 == 0)
+         	{
+         		input_btn(seat_btn[i],330+cnt+100, 132, 92, 92);
+             	seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
+             	seat_btn[i].setBackground(new Color(240, 240, 240));
+             	cnt += 210;
+         	}
+         	
+         }
+         
+         
+         else if(6 <= i && i <= 11)
+         {
+         	if(i % 2 == 0)
+         	{
+         		input_btn(seat_btn[i],330+cnt, 242, 92, 92);
+             	seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
+             	seat_btn[i].setBackground(new Color(240, 240, 240));
+             	cnt += 110;
+         	}
+         	
+         	
+         	else if(i % 2 != 0)
+         	{
+         		System.out.println(cnt);
+         		input_btn(seat_btn[i],330+cnt, 242, 92, 92);
+             	seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
+             	seat_btn[i].setBackground(new Color(240, 240, 240));
+             	cnt += 210;
+         	}
+         	
+         }
+         
+         else if(12 <= i && i <= 17)
+         {
+         	if(i % 2 == 0)
+         	{
+         		input_btn(seat_btn[i],330+cnt, 350, 92, 92);
+             	seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
+             	seat_btn[i].setBackground(new Color(240, 240, 240));
+             	cnt += 110;
+         	}
+         	
+         	
+         	else if(i % 2 != 0)
+         	{
+         		System.out.println(cnt);
+         		input_btn(seat_btn[i],330+cnt, 350, 92, 92);
+             	seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
+             	seat_btn[i].setBackground(new Color(240, 240, 240));
+             	cnt += 210;
+         	}
+         	
+         }
+         
+         else if(18 <= i && i <= 23)
+         {
+         	if(i % 2 == 0)
+         	{
+         		input_btn(seat_btn[i],330+cnt, 460, 92, 92);
+             	seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
+             	seat_btn[i].setBackground(new Color(240, 240, 240));
+             	cnt += 110;
+         	}
+         	
+         	
+         	else if(i % 2 != 0)
+         	{
+         		System.out.println(cnt);
+         		input_btn(seat_btn[i],330+cnt, 460, 92, 92);
+             	seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
+             	seat_btn[i].setBackground(new Color(240, 240, 240));;
+             	cnt += 210;
+         	}
+         	
+         }
+         
+         else
+         {
+         	if(i % 2 == 0)
+         	{
+         		input_btn(seat_btn[i],330+cnt, 570, 92, 92);
+             	seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
+             	seat_btn[i].setBackground(new Color(240, 240, 240));
+             	cnt += 110;
+         	}
+         	
+         	
+         	else if(i % 2 != 0)
+         	{
+         		System.out.println(cnt);
+         		input_btn(seat_btn[i],330+cnt, 570, 92, 92);
+             	seat_btn[i].setFont(new Font("맑은 고딕", Font.BOLD, 24));
+             	seat_btn[i].setBackground(new Color(240, 240, 240));
+             	cnt += 210;
+         	}
+         }
+
 		}
+		
 		
 		// 좌석 이미지 레이블
 		JLabel seat_image = new JLabel(new ImageIcon("./image/seat_image.png"));
 		seat_image.setBounds(150, 30, 1206, 743);
 		add(seat_image);
+		
 		
 		//이벤트 처리 추가
 		for(int i = 0; i < 30; i++)
@@ -219,11 +225,11 @@ public class Manager_menu extends JFrame{
 	         
 	         String reset = "UPDATE seat SET seatID = null, seatStart = null, seatEnd = null, seatRent = 0 WHERE seatID != 'null' and seatRent = 1";
 	         
-	         String del_res = "UPDATE seat SET seatID = null, seatRent = 0 WHERE seatNum = ? and seatRent = 1";
+	         String del_res = "UPDATE seat SET seatID = null, seatRent = 0, seatStart = null, seatEnd = null, WHERE seatNum = ? and seatRent = 1";
 	         
 	         PreparedStatement pstmt = null; //sql 실행
 	         PreparedStatement pstmt1 = null; //reset 실행
-             
+          
 	         try {
 	        	 	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 	        	 	String seatnum = seat_btn[i].getText();
@@ -231,24 +237,24 @@ public class Manager_menu extends JFrame{
 	        	 	pstmt.setString(1, seatnum);
 	        	 	ResultSet rs = pstmt.executeQuery(); //sql 실행 결과
 
-                    pstmt1 = conn.prepareStatement(reset); //reset 실행
-                    
+                 pstmt1 = conn.prepareStatement(reset); //reset 실행
+                 
 	        	 	while(rs.next()) {
 	        	 		if(rs.getString("seatRent").equals("1")) //좌석 대여 여부 1일 경우
 	        	 		{
 	        	 			seat_btn[i].setContentAreaFilled(true);
 	        	 			
 	        	 			Timestamp curr = new Timestamp(System.currentTimeMillis()); //현재시간 구하는 timestamp
-                     	   
-	        	 			String start = sdf.format(rs.getTime("seatStart")); //시작시간
+                  	   
+	        	 			//String start = sdf.format(rs.getTime("seatStart")); //시작시간
 	        	 			String currtime = sdf.format(curr); //현재시간
 	        	 			String end = sdf.format(rs.getTime("seatEnd")); //종료시간
 	        	 			
-	        	 			Date t_start = sdf.parse(start);
+	        	 			//Date t_start = sdf.parse(start);
 	        	 			Date t_curr = sdf.parse(currtime);
 	        	 			Date t_end = sdf.parse(end);
 	        	 			
-	        	 			long timeMil1 = t_start.getTime(); 
+	        	 			//long timeMil1 = t_start.getTime(); 
 	        	 			long timec = t_curr.getTime();
 	        	 			long timeMil2 = t_end.getTime();
 	        	 			
@@ -267,7 +273,7 @@ public class Manager_menu extends JFrame{
 	               }
 	         	}
 	         	catch(Exception e) {
-	         		System.out.println(e.toString());
+	         		System.out.println("이벤트 처리 X "+e.toString());
 	         	}
 	         
 	         seat_btn[i].addActionListener(new ActionListener() {
@@ -284,23 +290,26 @@ public class Manager_menu extends JFrame{
 	                        pstmt.setString(1, seatnum);
 	                        ResultSet rs = pstmt.executeQuery(); //sql 실행 결과
 	                        
-	                        pstmt2 = conn.prepareStatement(del_res); //cancle_res 실행
+	                        pstmt2 = conn.prepareStatement(del_res); //del_res 실행
 	                        pstmt2.setString(1, seatnum);
 	                        
 	                        if(seat_btn[i].equals(e.getSource()))
 	                        {
 	                           while(rs.next()) {
-	                        	   int result = JOptionPane.showConfirmDialog(null, "좌석을 삭제 하시겠습니까?", 
-	                        			   "Confirm", JOptionPane.YES_NO_OPTION);
-	                        	   if(result == JOptionPane.YES_OPTION)
+	                        	   if(rs.getString("seatRent").equals("1"))
 	                        	   {
-	                        		   pstmt2.executeUpdate();
-	                        		   JOptionPane.showMessageDialog(null, "좌석이 삭제 되었습니다.");
-	                        		   seat_btn[i].setContentAreaFilled(false);
-	                        	   }
-	                        	   else
-	                        	   {
-	                        		   JOptionPane.showMessageDialog(null, "좌석 삭제에 실패하였습니다");
+	                        		   int result = JOptionPane.showConfirmDialog(null, "좌석을 삭제 하시겠습니까?", 
+		                        			   "Confirm", JOptionPane.YES_NO_OPTION);
+		                        	   if(result == JOptionPane.YES_OPTION)
+		                        	   {
+		                        		   pstmt2.executeUpdate();
+		                        		   JOptionPane.showMessageDialog(null, "좌석이 삭제 되었습니다.");
+		                        		   seat_btn[i].setContentAreaFilled(false);
+		                        	   }
+		                        	   else
+		                        	   {
+		                        		   return;
+		                        	   }
 	                        	   }
 	                           	}
 	                        }      
@@ -312,7 +321,6 @@ public class Manager_menu extends JFrame{
 	            }
 	         });
 		}
-		
 		home_btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(db.logout())
@@ -320,32 +328,13 @@ public class Manager_menu extends JFrame{
 					db.seatlogout(); // 아이디 삭제
 					JOptionPane.showMessageDialog(null, "로그아웃 되었습니다.");
 				}
-				try {
-					 if(conn != null) {
-						 conn.close();
-						 System.out.println("닫기 성공");
-					 }
-				 }catch (SQLException e1) {
-					 e1.printStackTrace();
-				 }
-				db.dbclose();
-				new MainLogin(); //홈버튼을 누르면 첫 화면으로 이동
+				new MainLogin(); //홈 버튼을 누르면 첫 화면으로 이동
 				setVisible(false);
 			}
 		});
 		
 		user_management.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				try {
-					 if(conn != null) {
-						 conn.close();
-						 System.out.println("닫기 성공");
-					 }
-				 }catch (SQLException e1) {
-					 e1.printStackTrace();
-				 }
-				db.dbclose();
 				new User_Management(); //회원관리 버튼을 누르면 회원관리 페이지로 이동
 				setVisible(false);
 			}
@@ -353,16 +342,6 @@ public class Manager_menu extends JFrame{
 		
 		sales_check.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				try {
-					 if(conn != null) {
-						 conn.close();
-						 System.out.println("닫기 성공");
-					 }
-				 }catch (SQLException e1) {
-					 e1.printStackTrace();
-				 }
-				db.dbclose();
 				new Sales_Management(); //매출확인 버튼을 누르면 매출확인 페이지로 이동
 				setVisible(false);
 			}
@@ -370,16 +349,6 @@ public class Manager_menu extends JFrame{
 		
 		inquiry_check.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				try {
-					 if(conn != null) {
-						 conn.close();
-						 System.out.println("닫기 성공");
-					 }
-				 }catch (SQLException e1) {
-					 e1.printStackTrace();
-				 }
-				db.dbclose();
 				new inquiry_management(); //문의확인 버튼을 누르면 문의 확인 페이지로 이동
 				setVisible(false);
 			}
@@ -387,17 +356,6 @@ public class Manager_menu extends JFrame{
 		
 		setting_icon.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new manager_login(); //설정 버튼을 누르면 메인 로그인 페이지로 이동
-				setVisible(false);
-				try {
-					 if(conn != null) {
-						 conn.close();
-						 System.out.println("닫기 성공");
-					 }
-				 }catch (SQLException e1) {
-					 e1.printStackTrace();
-				 }
-				db.dbclose();
 				new manager_login(); //설정 버튼을 누르면 메인 로그인 페이지로 이동
 				setVisible(false);
 			}
@@ -413,39 +371,6 @@ public class Manager_menu extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // x를 누를 경우 종료
 		getContentPane().setBackground(Color.WHITE); // 프레임 bg color
 	
-	}
-	
-	public int db_amount() {
-		String sql = "select count(*) from seat where seatRent > 0;";
-		PreparedStatement pstmt = null;
-		int count = 0;
-		try {
-			pstmt = conn.prepareStatement(sql);
-			ResultSet rs = pstmt.executeQuery();
-			if(rs.next()) {
-				count = rs.getInt(1);
-			}
-		} catch (Exception e) {
-            System.out.println("select 메서드 예외발생");
-        }
-		return count;
-	}
-	
-	public void Rent_Check(int[] a) {
-		String sql = "select seatNum from seat where seatRent > 0;";
-        PreparedStatement pstmt = null;
-        try {
-            pstmt = conn.prepareStatement(sql);
-            ResultSet rs = pstmt.executeQuery();
-            int cnt = 0;
-            while (rs.next()) {
-              a[cnt] = rs.getInt("seatNum");
-              cnt++;
-            }
-  
-        } catch (Exception e) {
-            System.out.println("select 메서드 예외발생");
-        }
 	}
 	public static void main(String[] args) {
 		new Manager_menu();
